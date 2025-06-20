9dbca4cd5ccb21702f4559c43a814240e7fcccb4
ujala29 <aanyagupta980@gmail.com>
2025-06-20 21:43:01 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>You will import the pages and components here and do routing </h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/ClientDashoard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ClientDashoard = () => {
+  return (
+    <div>ClientDashoard</div>
+  )
+}
+
+export default ClientDashoard
