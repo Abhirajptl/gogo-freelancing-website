03059693dc1eb10d698401fb1c267025f29f1623
ujala29 <aanyagupta980@gmail.com>
2025-06-22 11:17:43 +0530

    Deleted folder-name from project

--- frontend/src/pages/ClientDashoard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ClientDashoard = () => {
-  return (
-    <div>ClientDashoard</div>
-  )
-}
-
-export default ClientDashoard
--- frontend/src/pages/FreelancerDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FreelancerDashboard = () => {
-  return (
-    <div>FreelancerDashboard</div>
-  )
-}
-
-export default FreelancerDashboard
